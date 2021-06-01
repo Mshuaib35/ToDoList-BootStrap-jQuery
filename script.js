e3ea0636adbf14abb20c48f9250a74ade999567c
Mshuaib35 <mohammedshuaib.35@gmail.com>
2021-06-01 13:28:27 +0530

    handle enter button events

--- script.js
+++ script.js
@@ -3,18 +3,29 @@ let btnAdd = $('#btnAdd')
 let btnClear = $('#btnClear')
 let inpNewTask = $('#inpNewTask')
 
-btnAdd.click(() => {
-  let listItem = $('<li>', {
-    'class': 'list-group-item',
-    text: inpNewTask.val()
-  })
+function addItem()
+{
+    
+        let listItem = $('<li>', {
+          'class': 'list-group-item',
+          text: inpNewTask.val()
+        })
+      
+        listItem.click(() => {
+          listItem.toggleClass('done')
+        })
+      
+        ulTasks.append(listItem)
+        inpNewTask.val('')
 
-  listItem.click(() => {
-    listItem.toggleClass('done')
-  })
-  
-  ulTasks.append(listItem)
-  inpNewTask.val('')
+    }
+
+   
+    inpNewTask.keypress((e) => {
+        if (e.which == 13) addItem()  
+
 })
+
+btnAdd.click(addItem)
 
 btnClear.click(() => inpNewTask.val(''))
